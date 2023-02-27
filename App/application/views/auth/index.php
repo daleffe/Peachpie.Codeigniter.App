ea44243d84cf2428cb6763fec4fab9c415ccd059
Guilherme Roberge Daleffe <daleffe@users.noreply.github.com>
2023-02-27 18:48:36 -0300

    Used helpers to generate HTML inputs

--- App/application/views/auth/index.php
+++ App/application/views/auth/index.php
@@ -1,4 +1,5 @@
-  <form method="POST">
+<?php echo form_open('auth', array('class' => 'form-signin')); ?>
+<?php echo form_fieldset(''); ?>
     <img class="mb-4" src="<?php echo asset_url('img/logo.png' , ''); ?>" width="72" height="57">
 
     <?php
@@ -6,18 +7,18 @@
         echo validation_errors('<div class="alert alert-danger" role="alert">','</div>');
     }
 
-    echo $this->session->flashdata('errors');
+    echo $this->session->flashdata('errors');    
     ?>
 
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email">
-      <label for="floatingInput">Email address</label>
+    <div class="form-floating">      
+      <?php echo form_input(array('id' => 'email', 'type' => 'email', 'name' => 'email', 'class' => 'form-control', 'placeholder' => 'name@example.com', 'required' => ''), set_value('email'), 'autofocus'); ?>
+      <label for="email">E-mail</label>
     </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
-      <label for="floatingPassword">Password</label>
+    <div class="form-floating">              
+      <?php echo form_password(array('id' => 'password', 'name' => 'password', 'class' => 'form-control', 'placeholder' => 'Password', 'required' => ''), set_value('password')); ?>
+      <label for="password">Password</label>
     </div>
 
     <div class="checkbox mb-3">
@@ -26,5 +27,5 @@
       </label>
     </div>
     <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
-  </form>
+<?php echo form_close(); ?>
+<p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
