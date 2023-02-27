9a8c8c477ddb1d5aefa079bcdc3b6f3c66770e1c
Guilherme Roberge Daleffe <daleffe@users.noreply.github.com>
2023-02-27 18:09:11 -0300

    Added form validation and session storage

--- App/application/controllers/auth.php
+++ App/application/controllers/auth.php
@@ -3,11 +3,30 @@
 class Auth extends CI_Controller {
 	
 	public function index()
-	{
+	{		
 		if ($this->input->method() == 'post') {
-			var_dump($this->input->post('email'));
-			var_dump($this->input->post('password'));
-		}
+			/* Form validation */
+			$this->form_validation->set_rules('email','e-mail',"trim|required|email");
+			$this->form_validation->set_rules('password','password',"trim|required|min_length[6]|max_length[60]");
+
+			if ($this->form_validation->run() == TRUE) {
+				$auth = array(
+					'email' => $this->input->post('email'),
+					'password' => $this->input->post('password')
+				);
+
+				$authorization = ($auth['email'] == 'test@test.com') && ($auth['password'] == '123456');
+
+				if ($authorization == false)  {
+					$this->session->set_flashdata('errors','<div class="alert alert-danger" role="alert">E-mail and/or password is invalid.</div>');
+				} else {		
+					// Save session data
+					$this->session->set_userdata($auth);
+
+					redirect('/','refresh');
+				}
+			}
+		}		
 
 		$this->load->view('layout', array('title' => 'Auth'));
 	}
