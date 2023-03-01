b408ea38ff0228a8c1ab29ac343c20d46c860367
Guilherme Roberge Daleffe <daleffe@users.noreply.github.com>
2023-02-28 21:48:18 -0300

    Added session alerts (ToDo: extract CSS class to config)

---
+++ App/application/libraries/MY_Session.php
@@ -0,0 +1,73 @@
+<?php
+
+class MY_Session extends CI_Session
+{	
+	var $_alert_key = 'alerts';
+
+    /* 
+	 * Alerts	 
+     */
+	public function has_alerts()
+	{		
+		return $this->flashdata($this->_alert_key) !== false || $this->userdata($this->flashdata_key.':new:'.$this->_alert_key) !== false;
+	}
+
+	public function output_alerts()
+	{
+		echo $this->alerts();
+	}
+
+	public function alerts()
+	{
+		$alerts = FALSE;
+
+		if ($this->flashdata($this->_alert_key) !== false)
+		{
+			$alerts = $this->flashdata($this->_alert_key);
+		} else if ($this->userdata($this->flashdata_key.':new:'.$this->_alert_key) !== false)
+		{
+			$alerts = $this->userdata($this->flashdata_key.':new:'.$this->_alert_key);
+
+			// Mark all new flashdata as old
+			$this->_flashdata_mark();
+
+			// Delete 'old' flashdata
+			$this->_flashdata_sweep();
+		}
+
+		return $alerts;
+	}
+
+	public function alert_notice($text = '')
+	{
+		$this->set_alert($text,'alert alert-info');
+	}
+
+	public function alert_success($text = '')
+	{
+		$this->set_alert($text,'alert alert-success');
+	}
+
+	public function alert_warning($text = '')
+	{
+		$this->set_alert($text,'alert alert-warning');
+	}
+
+	public function alert_error($text = '')
+	{
+		$this->set_alert($text,'alert alert-danger');
+	}
+
+	public function set_alert($text = '', $class = 'alert alert-info')
+	{
+		if (!empty($text))
+		{			
+			$this->set_flashdata($this->_alert_key,$this->_get_alert($text,$class));			
+		}		
+	}	
+
+	private function _get_alert($text, $class = 'alert alert-primary')
+	{		
+		return '<div' . (empty(trim($class)) ? '' : ' class="' . $class . '"') . ' role="alert">' . trim($text) . '</div>';
+	}
+}
